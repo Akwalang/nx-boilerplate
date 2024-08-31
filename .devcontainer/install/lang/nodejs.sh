1271d4ad72a691e12ae89ab818b47d8f21bcfac1
Siarhei Kukunin <siarhei.kukunin@btbisrael.co.il>
2024-08-31 23:34:50 +0200

    feat: install node.js

--- .devcontainer/install/lang/nodejs.sh
+++ .devcontainer/install/lang/nodejs.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-Log "$(Purple "Node.js") Begin installation"
-Log "$(Purple "Node.js") Installation completed"
+NAME="Node.js 22"
+
+Log "$(Purple "$NAME") Begin installation"
+
+curl -sL https://deb.nodesource.com/setup_22.x | bash -
+apt-get install -y nodejs
+
+Log "$(Purple "$NAME") Install TypeScript"
+
+npm install -g typescript
+
+Log "$(Purple "$NAME") Installation completed"
