ccd22e387dca3dc0e39c114eb50b22e1a051d868
Siarhei Kukunin <siarhei.kukunin@btbisrael.co.il>
2024-09-02 00:20:16 +0200

    feat: update rust install script

--- .devcontainer/scripts/install/lang/rust.sh
+++ .devcontainer/scripts/install/lang/rust.sh
@@ -8,14 +8,27 @@ install_lang_rust() {
 
   Log "$(Purple "$NAME"): Begin installation"
 
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y >> $LOG_PATH 2>&1
-  rustup component add rust-analyzer >> $LOG_PATH 2>&1
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 2>&1 | while IFS= read -r line; do
+    if [[ "$line" = *"info: installing component"* ]]; then
+      Log "$(Purple "$NAME"): $(Yellow "${line/"info: installing"/"add"}")"
+    fi
+    echo "$line" >> "$LOG_PATH"
+  done
+
+  cd $HOME/.cargo/bin
+
+  ./rustup component add rust-analyzer 2>&1 | while IFS= read -r line; do
+    if [[ "$line" = *"info: installing component"* ]]; then
+      Log "$(Purple "$NAME"): $(Yellow "${line/"info: installing"/"add"}")"
+    fi
+    echo "$line" >> "$LOG_PATH"
+  done
 
   echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
 
   source ~/.bashrc
 
-  Log "$(Purple "$NAME"): $(Yellow "$(rustc --version)")"
+  Log "$(Purple "$NAME"): $(Yellow "$(./rustc --version)")"
 
   Log "$(Purple "$NAME"): Installation completed"
 }
