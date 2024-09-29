5afe24f0f80946e6f43b0de216dded9252d32948
Siarhei Kukunin <siarhei.kukunin@btbisrael.co.il>
2024-09-29 11:48:22 +0000

    feat: update nest generator

--- .config/nx/plugins/nodejs/nest/src/generators/app/files/test/fakers/comment.ts
+++
@@ -1,9 +0,0 @@
-import { randomString } from '../utils/randomString';
-
-export const fakeComment = (): { message: string } => {
-  const rand = randomString(10);
-
-  return {
-    message: `Comment content <b>${rand}</b>`,
-  };
-};
